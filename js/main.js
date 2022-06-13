b5754c0924628bf3d97368fe77afc49d8742f344
Amy Baker <amyraebaker@gmail.com>
2022-06-13 00:14:00 -0700

    Successfully created grid with borders

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const container = document.querySelector('#container');
 
-for (let i = 1; i <= 16; i++) {
-    const divs = document.createElement('div');
-    divs.classList.add('square');
-    container.appendChild(divs);
+for (let i = 1; i <= 16 * 16; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
 };
+
+
 
 
 
