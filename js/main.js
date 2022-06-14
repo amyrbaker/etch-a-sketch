ff911ece6a42dca987926c08db877120ee552e1b
Amy Baker <amyraebaker@gmail.com>
2022-06-13 17:34:08 -0700

    Create board reset and update event listener to squares

--- js/main.js
+++ js/main.js
@@ -8,17 +8,20 @@ function createBoard(size) {
         square.style.width = `${512 / size - 2}px`;
         square.style.height = `${512 / size - 2}px`;
         container.appendChild(square);
+        square.addEventListener('mouseover', function(e) {
+            e.target.style.background = 'grey';
+        });
     };
 };
+createBoard(16)
 
-container.addEventListener('mouseover', function(e) {
-    e.target.style.background = 'grey';
-});
+
 
 button.addEventListener('click', () => {
     let boardSize = prompt('Choose a board size');
     while (boardSize > 100) boardSize = prompt('Board size cannot be greater than 100. Choose a board size');
-    reset('#container')
+    reset(container)
+    container.style.background = 'white'
     createBoard(boardSize);
 });
 
