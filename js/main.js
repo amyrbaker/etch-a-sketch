100762eeccfd9d3b25e721aad7d9ac31296ac656
Amy Baker <amyraebaker@gmail.com>
2022-06-13 17:27:26 -0700

    Add logic to reset board size based on user input

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('button');
 
-for (let i = 1; i <= 16 * 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+function createBoard(size) {
+    for (let i = 1; i <= size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${512 / size - 2}px`;
+        square.style.height = `${512 / size - 2}px`;
+        container.appendChild(square);
+    };
 };
 
 container.addEventListener('mouseover', function(e) {
-    console.log(e.target)
     e.target.style.background = 'grey';
-    
 });
+
+button.addEventListener('click', () => {
+    let boardSize = prompt('Choose a board size');
+    while (boardSize > 100) boardSize = prompt('Board size cannot be greater than 100. Choose a board size');
+    reset('#container')
+    createBoard(boardSize);
+});
+
+function reset(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+
 
 
 
